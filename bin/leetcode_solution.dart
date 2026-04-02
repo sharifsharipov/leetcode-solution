92de99a36fa7819bbdc2238789ec1678fb15a23f
Sharif <sharifsharipov2003@gmail.com>
2026-04-02 18:54:05 +0500

    intial commit

---
+++ bin/leetcode_solution.dart
@@ -0,0 +1,5 @@
+import 'package:leetcode_solution/leetcode_solution.dart' as leetcode_solution;
+
+void main(List<String> arguments) {
+  print('Hello world: ${leetcode_solution.calculate()}!');
+}
---
+++ test/leetcode_solution_test.dart
@@ -0,0 +1,8 @@
+import 'package:leetcode_solution/leetcode_solution.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
